9d1cac5b9978908d48f8a452df628dfbe6874b26
Julien Fontanet <julien.fontanet@isonoe.net>
2019-02-22 19:52:48 +0100

    feat(@xen-orchestra/{backup/xapi}): new packages

--- @xen-orchestra/xapi/src/sr.js
+++ @xen-orchestra/xapi/src/sr.js
@@ -2,18 +2,38 @@ const groupBy = require('lodash/groupBy')
 const keyBy = require('lodash/keyBy')
 
 module.exports = class Sr {
-  async getUnhealthyVdiChainsLength(ref) {
+  async getUnhealthyVdiChainsLength(ref = this.pool.default_SR) {
     const vdiRefs = await this.getField('SR', ref, 'VDIs')
     const vdis = await this.getRecords('VDI', vdiRefs)
     const vdiByUuid = keyBy(vdis, 'uuid')
 
     const children = { __proto__: null }
-    vdis.forEach(vdi => {
+    vdiRefs.forEach(ref => {
+      const vdi = vdis[ref]
       const parent = vdi.sm_config['vhd-parent']
       if (parent !== undefined) {
-        (children[parent] ?? (children[parent] = [])).push(vdi.$ref)
+        (children[parent] ?? (children[parent] = [])).push(vdi.uuid)
       }
     })
+
+
+    const chainLengthByVdi = { __proto__: null }
+    const computeChainLength = uuid
+
+    const n = vdiRefs.length
+    let i = 0
+    while (i < n) {
+      const ref = vdiRefs[i]
+      const vdi = vdis[ref]
+      const parent = vdi.sm_config['vhd_parent']
+      let chainLength = 0
+      if (parent !== undefined) {
+        const parentChainLength = chainLengthByVdi[parent]
+        if (parentChainLength === undefined) {
+
+        }
+      }
+    }
 
     return children
   }
